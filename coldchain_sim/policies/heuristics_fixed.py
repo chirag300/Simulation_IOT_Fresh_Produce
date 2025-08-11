ff836d46a18b7a215dc04514d93837ce99cf0056
Chirag Seth <91310725+chirag300@users.noreply.github.com>
2025-08-10 18:40:53 -0700

    Create heuristics_fixed.py

---
+++ coldchain_sim/policies/heuristics_fixed.py
@@ -0,0 +1,36 @@
+import networkx as nx
+from .base import RoutePolicy
+
+class NearestNeighborPolicy(RoutePolicy):
+    def build_route(self, G, depot, customers):
+        unvisited = set(customers)
+        route = [depot]
+        cur = depot
+        while unvisited:
+            nxt = min(unvisited, key=lambda j: G[cur][j]["time"])
+            route.append(nxt)
+            unvisited.remove(nxt)
+            cur = nxt
+        route.append(depot)
+        return route
+
+
+def two_opt(route, G):
+    best = route[:]
+    improved = True
+    while improved:
+        improved = False
+        for i in range(1, len(best) - 2):
+            for j in range(i + 1, len(best) - 1):
+                new = best[:i] + best[i:j][::-1] + best[j:]
+                if path_time(new, G) < path_time(best, G):
+                    best = new
+                    improved = True
+    return best
+
+
+def path_time(route, G):
+    t = 0
+    for a, b in zip(route, route[1:]):
+        t += G[a][b]["time"]
+    return t
